29370a805ea987b65560ac0b5d3ade13955730d9
lazzieeCoder <rupanrrc26@gmail.com>
2023-09-04 10:33:31 +0530

    base code

---
+++ script.js
@@ -0,0 +1,16 @@
+//!initial values
+
+
+//!get method(Retriving the data)
+
+
+//!insert the data (Post method)
+
+
+//!edit and update the data (Update method)
+
+
+//!deleting the data (delete method)
+
+
+//!reseting the values in form
